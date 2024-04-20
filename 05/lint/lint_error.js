5ebf170f9ad1ecc0111a85bbac1602d151e565e5
Cheese <cheese@Cheeseui-MacBookPro.local>
2024-04-20 19:50:10 +0900

    예제 실습 파일 수정

--- 05/lint/lint_error.js
+++ 05/lint/lint_error.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-console */
-const name = 'John Doe';
-const age = 30;
+var name = 'John Doe';
+var age = 30;
+var unusedVariable = 'This is not used anywhere';
 
-if (name === 'John Doe') {
+if (name == 'John Doe') {
   console.log('Welcome, John Doe!');
 }
 
 console.log('This is a log message');
 
-const greeting = `Hello, ${name}! You are ${age} years old`;
+var greeting = `Hello, ${name}! You are ${age} years old`;
 
 if (age > 18) {
   console.log(greeting);
